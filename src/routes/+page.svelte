70142221b8e82810ed5fa0cb89ae886913554bb1
AlwaysWannaDie12 <srenjoy.saha12@gmail.com>
2024-06-17 19:18:32 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<main class="h-screen w-screen flex items-center justify-center flex-col gap-4">
+  
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<ul>
+	<li>Button</li>
+	<li>Input</li>
+	<li>Label</li>
+	<li>Checkbox</li>
+</ul>
+<Button>Join Now</Button>  
+</main>
+
